f508af8e4f805a5b0f134a752f450266b3ab1a98
keiba804 <mottuso0017@gmail.com>
2020-09-12 13:51:51 +0000

    finish 7.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(user_params)
   	if @user.save
   	else
   		render 'new'
@@ -19,4 +19,5 @@ class UsersController < ApplicationController
 
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
 end
