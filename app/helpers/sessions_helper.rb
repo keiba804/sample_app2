4c90c8efd0c2382cdb6521518a82d7f554a1d38b
keiba804 <mottuso0017@gmail.com>
2020-09-18 04:48:23 +0000

    finish 8.1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,2 @@
 module SessionsHelper
-
-	def new
-	end
-
-	def create
-		render 'new'
-	end
-
-	def destroy
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get 'login',     to: 'sessions#new'
-  get 'login',     to: 'sessions#create'
+  post 'login',     to: 'sessions#create'
   get 'logout',     to: 'sessions#destroy'
   resources :users
 end
