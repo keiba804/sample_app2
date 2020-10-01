ac31af0dcfaab4ccdd2074166b5fed943a30e41d
keiba804 <mottuso0017@gmail.com>
2020-10-01 00:01:07 +0000

    finish 9.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
 
 	# 渡されたトークンがダイジェストと一致したらtrueを返す
 	def authenticated?(remember_token)
+		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 
@@ -35,4 +36,6 @@ class User < ApplicationRecord
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+
 end
